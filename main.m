a736c66b857165d518f29fdd19af3f6b7b956c5d
yixin <yixin.wang@cern.ch>
2019-09-20 15:48:31 +0800

    best path

--- main.m
+++ main.m
@@ -2,30 +2,237 @@ clear;close all;
 
 [data1, datac1] = data_prep('data1.csv',306);
 [data2, datac2] = data_prep('data2.csv',167);
- 
-% save data
-writematrix(data1, 'data1.csv');
-writematrix(datac1, 'datac1.csv');
-writematrix(data2, 'data2.csv');
-writematrix(datac2, 'datac2.csv');
+N1 = length(data1);N2 = length(data2);
+% % save data
+% writematrix(data1, 'data1.csv');
+% writematrix(datac1, 'datac1.csv');
+% writematrix(data2, 'data2.csv');
+% writematrix(datac2, 'datac2.csv');
 
 
 [graph1,W1] = build_graph(datac1,25,15,20,25,30,0.001);
 [graph2,W2] = build_graph(datac2,20,10,15,20,20,0.001);
 
-% save graph
-writematrix(graph1,'graph1.csv');
-writematrix(graph2,'graph2.csv');
+% % save graph
+% writematrix(graph1,'graph1.csv');
+% writematrix(graph2,'graph2.csv');
 
-[path1, distance1] = cal_shortestpath(graph1, W1)
-[path2, distance2] = cal_shortestpath(graph2, W2)
-% D1 = sqrt(datac1(304,2)^2 + datac1(304,3)^2 + datac1(304,4)^2) ...
-% + sqrt((datac1(344,2)-datac1(304,2))^2 + (datac1(344,3)-datac1(304,3))^2 + (datac1(344,4)-datac1(304,4))^2) ...
-% + sqrt((datac1(172,2)-datac1(344,2))^2 + (datac1(172,3)-datac1(344,3))^2 + (datac1(172,4)-datac1(344,4))^2) ...
-% + sqrt((datac1(327,2)-datac1(172,2))^2 + (datac1(327,3)-datac1(172,3))^2 + (datac1(327,4)-datac1(172,4))^2);
+% [path1, distance1] = cal_THEshortestpath(graph1, W1, 'positive')
+% [path2, distance2] = cal_THEshortestpath(graph2, W2, 'positive')
+
+%k1 = 
+k2 = 300; % param to change
+
+%[bestPath1, shortestPaths1, leastNodes1] = get_bestPath(graph1, W1, N1, k1)
+[bestPath2, shortestPaths2, leastNodes2] = get_bestPath(graph2, W2, N2, k2)
+
+function [bestPath, shortestPaths, leastNodes] = get_bestPath(linkMatrix, weightMatrix,N,k)
+
+[leastNodes] = kShortestPath(linkMatrix, 1, N, k);
+[shortestPaths, totalCosts] = kShortestPath(weightMatrix, 1, N, k);
+
+bestPath = [];
+s = 1;
+for i = 1:k
+    for j = 1:k
+       if  isequal(shortestPaths{i},leastNodes{j})
+           bestPath{s} = shortestPaths{i};
+           s = s + 1;
+       end
+    end
+    
+end
+
+end
 
 
-function [path, distance] = cal_shortestpath(affinity, W)
+function [shortestPaths, totalCosts] = kShortestPath(netCostMatrix, source, destination, k_paths)
+ 
+if source > size(netCostMatrix,1) || destination > size(netCostMatrix,1)
+    warning('The source or destination node are not part of netCostMatrix');
+    shortestPaths=[];
+    totalCosts=[];
+else
+    %---------------------INITIALIZATION---------------------
+    k=1;
+    [path cost] = dijkstra(netCostMatrix, source, destination);
+    %P is a cell array that holds all the paths found so far:
+    if isempty(path)
+        shortestPaths=[];
+        totalCosts=[];
+    else
+        path_number = 1; 
+        P{path_number,1} = path; P{path_number,2} = cost; 
+        current_P = path_number;
+        %X is a cell array of a subset of P (used by Yen's algorithm below):
+        size_X=1;  
+        X{size_X} = {path_number; path; cost};
+ 
+        %S path_number x 1
+        S(path_number) = path(1); %deviation vertex is the first node initially
+ 
+        % K = 1 is the shortest path returned by dijkstra():
+        shortestPaths{k} = path ;
+        totalCosts(k) = cost;
+ 
+        %--------------------------------------------------------
+        while (k < k_paths   &&   size_X ~= 0 )
+            %remove P from X
+            for i=1:length(X)
+                if  X{i}{1} == current_P
+                    size_X = size_X - 1;
+                    X(i) = [];%delete cell
+                    break;
+                end
+            end
+ 
+            %---------------------------------------
+            P_ = P{current_P,1}; %P_ is current P, just to make is easier for the notations
+ 
+            %Find w in (P_,w) in set S, w was the dev vertex used to found P_
+            w = S(current_P);
+            for i = 1: length(P_)
+                if w == P_(i)
+                    w_index_in_path = i;
+                end
+            end
+ 
+ 
+            for index_dev_vertex= w_index_in_path: length(P_) - 1   %index_dev_vertex is index in P_ of deviation vertex
+                temp_netCostMatrix = netCostMatrix;
+                %------
+                %Remove vertices in P before index_dev_vertex and there incident edges
+                for i = 1: index_dev_vertex-1
+                    v = P_(i);
+                    temp_netCostMatrix(v,:)=inf;
+                    temp_netCostMatrix(:,v)=inf;
+                end
+                %------
+                %remove incident edge of v if v is in shortestPaths (K) U P_  with similar sub_path to P_....
+                SP_sameSubPath=[];
+                index =1;
+                SP_sameSubPath{index}=P_;
+                for i = 1: length(shortestPaths)
+                    if length(shortestPaths{i}) >= index_dev_vertex
+                        if P_(1:index_dev_vertex) == shortestPaths{i}(1:index_dev_vertex)
+                            index = index+1;
+                            SP_sameSubPath{index}=shortestPaths{i};
+                        end
+                    end            
+                end       
+                v_ = P_(index_dev_vertex);
+                for j = 1: length(SP_sameSubPath)
+                    next = SP_sameSubPath{j}(index_dev_vertex+1);
+                    temp_netCostMatrix(v_,next)=inf;   
+                end
+                %------
+ 
+                %get the cost of the sub path before deviation vertex v
+                sub_P = P_(1:index_dev_vertex);
+                cost_sub_P=0;
+                for i = 1: length(sub_P)-1
+                    cost_sub_P = cost_sub_P + netCostMatrix(sub_P(i),sub_P(i+1));
+                end
+ 
+                %call dijkstra between deviation vertex to destination node    
+                [dev_p c] = dijkstra(temp_netCostMatrix, P_(index_dev_vertex), destination);
+                if ~isempty(dev_p)
+                    path_number = path_number + 1;
+                    P{path_number,1} = [sub_P(1:end-1) dev_p] ;  %concatenate sub path- to -vertex -to- destination
+                    P{path_number,2} =  cost_sub_P + c ;
+ 
+                    S(path_number) = P_(index_dev_vertex);
+ 
+                    size_X = size_X + 1; 
+                    X{size_X} = {path_number;  P{path_number,1} ;P{path_number,2} };
+                else
+                    %warning('k=%d, isempty(p)==true!\n',k);
+                end      
+            end
+            %---------------------------------------
+            %Step necessary otherwise if k is bigger than number of possible paths
+            %the last results will get repeated !
+            if size_X > 0
+                shortestXCost= X{1}{3};  %cost of path
+                shortestX= X{1}{1};        %ref number of path
+                for i = 2 : size_X
+                    if  X{i}{3} < shortestXCost
+                        shortestX= X{i}{1};
+                        shortestXCost= X{i}{3};
+                    end
+                end
+                current_P = shortestX;
+                %******
+                k = k+1;
+                shortestPaths{k} = P{current_P,1};
+                totalCosts(k) = P{current_P,2};
+                %******
+            else
+                %k = k+1;
+            end
+        end
+    end
+end
+end
+ 
+function [shortestPath, totalCost] = dijkstra(netCostMatrix, s, d)
+ 
+n = size(netCostMatrix,1);
+for i = 1:n
+    % initialize the farthest node to be itself;
+    farthestPrevHop(i) = i; % used to compute the RTS/CTS range;
+    farthestNextHop(i) = i;
+end
+ 
+% all the nodes are un-visited;
+visited(1:n) = false;
+ 
+distance(1:n) = inf;    % it stores the shortest distance between each node and the source node;
+parent(1:n) = 0;
+ 
+distance(s) = 0;
+for i = 1:(n-1),
+    temp = [];
+    for h = 1:n,
+         if ~visited(h)  % in the tree;
+             temp=[temp distance(h)];
+         else
+             temp=[temp inf];
+         end
+     end;
+     [t, u] = min(temp);      % it starts from node with the shortest distance to the source;
+     visited(u) = true;         % mark it as visited;
+     for v = 1:n,                % for each neighbors of node u;
+         if ( ( netCostMatrix(u, v) + distance(u)) < distance(v) )
+             distance(v) = distance(u) + netCostMatrix(u, v);   % update the shortest distance when a shorter shortestPath is found;
+             parent(v) = u;     % update its parent;
+         end;             
+     end;
+end;
+ 
+shortestPath = [];
+if parent(d) ~= 0   % if there is a shortestPath!
+    t = d;
+    shortestPath = [d];
+    while t ~= s
+        p = parent(t);
+        shortestPath = [p shortestPath];
+        
+        if netCostMatrix(t, farthestPrevHop(t)) < netCostMatrix(t, p)
+            farthestPrevHop(t) = p;
+        end;
+        if netCostMatrix(p, farthestNextHop(p)) < netCostMatrix(p, t)
+            farthestNextHop(p) = t;
+        end;
+ 
+        t = p;      
+    end;
+end;
+ 
+totalCost = distance(d);
+end
+
+function [path, distance] = cal_THEshortestpath(affinity, W, methods)
 % *********************calculate shortest path**********************************    
 N = length(affinity);
 s = [];
@@ -47,7 +254,7 @@ G = digraph(s,t,w);
 % p = plot(G,'EdgeLabel',G.Edges.Weight);
 
 %Dijkstra:"positive" Bellman-Ford:"mixed"
-[path, distance] = shortestpath(G,1,N,'Method', "positive");
+[path, distance] = shortestpath(G,1,N,'Method', methods);
 % highlight(p, path,'EdgeColor','red')
 
 
@@ -58,7 +265,7 @@ function [graph,W] = build_graph(data,alpha1,alpha2,beta1,beta2,theta,delta)
 %*********************build graph*****************************************
 
 n = length(data);
-graph = zeros(n);
+graph = Inf(n);
 W = Inf(n);
 
 alpha = min(alpha1,alpha2);
