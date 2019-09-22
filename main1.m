09670ab7b3b6515bb309f1a023e142e36b37510b
yixin <yixin.wang@cern.ch>
2019-09-23 03:07:37 +0800

    final

--- main1.m
+++ main1.m
@@ -23,7 +23,7 @@ P1 = zeros(N1); P2 = zeros(N2);% init
 % writematrix(data2, 'data2.csv');
 % writematrix(datac2, 'datac2.csv');
 
-%% Q1 --------------------------------------------
+%% Q1 
 a = 1; b = 1e4; c = 0;
 
 [G1,W1] = build_graph(datac1,25,15,20,25,30,0.001);
@@ -39,7 +39,7 @@ a = 1; b = 1e4; c = 0;
 % writecell(bestDistancePaths1,'BestDistancePaths1.csv');
 % writecell(bestDistancePaths2,'BestDistancePaths2.csv');
 
-%% Q2 --------------------------------------------
+%% Q2 
 % a = 1; b = 1e4; c = 0;
 
 [nG1, nW1] = build_strict_graph(datac1, G1, W1, N1);
@@ -55,7 +55,7 @@ a = 1; b = 1e4; c = 0;
 % writecell(bestDNPaths1,'BestDNPaths1.csv');
 % writecell(bestDNPaths2,'BestDNPaths2.csv');
 
-%% Q3 --------------------------------------------
+%% Q3 
 a = 1; b = 1e4; c = 44800;
 
 P1 = build_graphProb(datac1, G1, N1, alpha11,alpha21,beta11,beta21,delta1);
@@ -71,7 +71,71 @@ P2 = build_graphProb(datac2, G2, N2, alpha12,alpha22,beta12,beta22,delta2);
 % writecell(bestPaths1,'BestPaths1.csv');
 % writecell(bestPaths2,'BestPaths2.csv');
 
-%% functions
+%% Plot paths
+
+datafile1 = 'data1.csv'; datafile2 = 'data2.csv';
+flag1 = 167; flag2 = 306;
+
+BestDistancePaths1 = 'BestDistancePaths1.csv';
+BestDistancePaths2 = 'BestDistancePaths2.csv';
+BestDNPaths1 = 'BestDNPaths1.csv';
+BestDNPaths2 = 'BestDNPaths2.csv';
+BestPaths1 = 'BestPaths1.csv';
+BestPaths2 = 'BestPaths2.csv';
+
+d11 = plot_result(datafile1, BestDistancePaths1, flag1,'Answer1 for data1',1);
+d12 = plot_result(datafile2, BestDistancePaths2, flag2,'Answer1 for data2',2);
+
+d21 = plot_result(datafile1, BestDNPaths1, flag1,'Answer2 for data1',3);
+d22 = plot_result(datafile2, BestDNPaths2, flag2,'Answer2 for data2',4);
+
+d31 = plot_result(datafile1, BestPaths1, flag1,'Answer3 for data1',5);
+d32 = plot_result(datafile2, BestPaths2, flag2,'Answer3 for data2',6);
+
+%% Functions
+
+function d = plot_result(datafile, pathfile, flag, mytitle,i)
+
+data = csvread(datafile); 
+path = csvread(pathfile);
+
+A = data(1,:);
+B = data(end,:);
+dataP = sortrows(data(2:end-1,:),5);
+figure;
+scatter3(A(2),A(3),A(4),'r','o','filled'); %A
+hold on;
+scatter3(B(2),B(3),B(4),'r','o','filled'); %B
+hold on;
+scatter3(dataP(1:flag,2),dataP(1:flag,3),dataP(1:flag,4),'.','m');
+hold on;
+scatter3(dataP(flag+1:end,2),dataP(flag+1:end,3),dataP(flag+1:end,4),'.','b');
+hold on;
+plot3([A(2),B(2)],[A(3),B(3)],[A(4),B(4)],'k--');
+hold on;
+d = [];
+ for i = 3: path(1,end)+1
+    hold on;
+    % calculate distance error
+    d(i-2) = 0.001*sqrt( (data(path(1,i+1),2) - data(path(1,i),2))^2 ...
+            +(data(path(1,i+1),3) - data(path(1,i),3))^2 ...
+            +(data(path(1,i+1),4) - data(path(1,i),4))^2 );
+    plot3([data(path(1,i),2),data(path(1,i+1),2)],...
+       [data(path(1,i),3),data(path(1,i+1),3)],...
+       [data(path(1,i),4),data(path(1,i+1),4)],'k'); 
+
+ end
+
+text(A(2),A(3),A(4),'  A', 'fontsize', 14);
+text(B(2),B(3),B(4),'  B', 'fontsize', 14);
+xlabel('x');
+ylabel('y');
+zlabel('z');
+title(mytitle);
+
+saveas(gcf,strcat(mytitle,'.eps'))
+end
+
 function nnG = build_graphProb(datac, graph, N, alpha1,alpha2,beta1,beta2,delta)
 
 nnG = graph;
